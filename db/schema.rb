3e2ac993c43cb8fc53a74cb32fbbf57897284dfa
AmiasYaska <amiasyaska@gmail.com>
2025-01-29 16:07:12 +0300

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_29_124808) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_29_130512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_29_124808) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "headline"
+    t.string "website"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
