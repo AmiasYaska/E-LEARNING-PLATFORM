09d2a205bcd81521b637d13045656976d35f4186
AmiasYaska <amiasyaska@gmail.com>
2025-01-31 15:52:31 +0300

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resource :profile, only: [:show, :edit, :update]
-  resources :videos, only: [:index, :show]
+
+  resources :videos, only: [:index, :show] do
+    resource :enrollment, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
