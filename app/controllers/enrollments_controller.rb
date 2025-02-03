3394565019f8683e4dea0dfb59b6f5e878d4abd8
AmiasYaska <amiasyaska@gmail.com>
2025-02-03 12:59:49 +0300

    changes

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,7 +3,7 @@ class EnrollmentsController < ApplicationController
 
     def create
         @video = Video.find(params.expect(:video_id))
-        current_user.enrollments.create(video: @video)
+        current_user.enrollments.create(video: @video, progress: 0)
         redirect_to video_path(@video), notice: "Enrolled successfully", status: :see_other
     end
 
--- app/javascript/controllers/video_player_controller.js
+++ app/javascript/controllers/video_player_controller.js
@@ -1,10 +1,43 @@
 import { Controller } from "@hotwired/stimulus"
-import Plyr from "plyr"
+import Plyr from 'plyr'
 
 export default class extends Controller {
+  static values = { videoId: Number }
+
   connect() {
-    new Plyr("#player", {
+    this.player = new Plyr('#player', {
       autoplay: true
+    })
+
+    // Load saved progress
+    this.player.on('loadedmetadata', () => {
+      this.fetchProgress().then(progress => {
+        if (progress) this.player.currentTime = this.player.duration * progress
+      })
+    })
+
+    // Save progress every 15 seconds
+    this.player.on('timeupdate', () => {
+      if (this.player.currentTime % 5 < 0.25) { // Throttle saves
+        this.saveProgress(this.player.currentTime / this.player.duration)
+      }
+    })
+  }
+
+  async fetchProgress() {
+    const response = await fetch(`/videos/${this.videoIdValue}/progress`)
+    const data = await response.json()
+    return data.progress
+  }
+
+  async saveProgress(progress) {
+    await fetch(`/videos/${this.videoIdValue}/progress`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': document.querySelector("[name='csrf-token']").content
+      },
+      body: JSON.stringify({ progress: progress })
     })
   }
 }
