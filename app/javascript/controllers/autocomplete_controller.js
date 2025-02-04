c02afb72b71f0a49985b9fd4a9e7b5316d965bbf
AmiasYaska <amiasyaska@gmail.com>
2025-02-04 22:28:45 +0300

    changes

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -2,14 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = ["input", "results"]
-    connect () {
+
+    connect() {
         this.timeout = null
     }
 
     search() {
         clearTimeout(this.timeout)
-        this.timeout = setTimeout(() => { 
+        this.timeout = setTimeout(() => {
             this.fetchResults(this.inputTarget.value)
         }, 300)
+    }
+
+    async fetchResults(query) {
+        if(query.length < 2) {
+            this.inputTarget.innerHTML = ""
+        }
+
+        try {
+            response = await fetch("/videos/autocomplete?query=${encodeURIComponent(query)}")
+            results = await response.json()
+            this.displayResults(results)
+        } catch(error) {
+            console.log("Incomplete failed", error)
+        }
+        
     }
 }
